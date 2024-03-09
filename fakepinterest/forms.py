01d1884a0fda9086a9a60181c15d83fa4bfecd0e
rmtrescato <rmenezestrescato@gmail.com>
2024-03-09 20:28:18 -0300

    inicio do forms

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,16 @@
 # formulario do site
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+class FormLogin(FlaskForm):
+   email = StringField()
+   senha =
+   botao_confirmacao = 
+   
+  
+class FormCriarConta(FlaskForm):
+   email = 
+   username = 
+   senha = 
+   confirmacao_senha = 
+  
