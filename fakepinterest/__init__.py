9347e1d9c7105f0c7cf6b6c710a271a8d0569b65
rmtrescato <rmenezestrescato@gmail.com>
2024-03-09 20:00:38 -0300

    Incluso no projeto atributos sistema de login e seguranca

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "64ca34b4d1ca3fc1fe07dc3f92538487"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fakepinterest import routes
