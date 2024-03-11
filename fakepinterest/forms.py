e7e8987fed09ad8d5f3adc0c7497fc073bc2b486
rmtrescato <rmenezestrescato@gmail.com>
2024-03-11 10:14:02 -0300

    Implementacao do fomrs de login na home

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,16 +1,24 @@
 # formulario do site
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
 
 class FormLogin(FlaskForm):
-   email = StringField()
-   senha =
-   botao_confirmacao = 
+   email = StringField("E-mail", validators=[DataRequired(), Email()])
+   senha = PasswordField("Senha", validators=[DataRequired()])
+   botao_confirmacao = SubmitField("Fazer Login")
    
   
 class FormCriarConta(FlaskForm):
-   email = 
-   username = 
-   senha = 
-   confirmacao_senha = 
-  
+   email = StringField("E-mail", validators=[DataRequired(), Email()])
+   username = StringField("Nome de Usuario", validators=[Datarequired()])
+   senha = PasswordField("Senha", validators=[DataRequeired(), Length(6, 20)])
+   confirmacao_senha = PasswordField("Conformacao de Senha", validators=[DataRequired(), EqualTo("senha")])
+   botao_confirmacao = SubmitField("Criar Conta")
+
+   def validate_email(self, email):
+      usuario = Usuario.query.filter_by(email=email.data).first()
+      if usuario:
+         return ValidationError("Email ja cadastrado, utilize outro para realizar o seu login.")
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -2,12 +2,19 @@
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest import FormLogin, FormCriarConta
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-   return render_template('index.html')
+   formLogin = FormLogin()
+   return render_template('index.html', form=formlogin)
 
- 
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+   formcriarconta = FormCriarConta()
+   return render_template("criarconta.html", form=formcriarconta)
+
 @app.route("/perfil/<usuario>")
 @login_required
 def perfil(usuario):
