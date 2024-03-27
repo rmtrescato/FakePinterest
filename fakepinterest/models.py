fbc0eb2bca801705dd2b343a69f5193860e0011e
rmtrescato <rmenezestrescato@gmail.com>
2024-03-27 09:07:51 -0300

    criacao de login e logout

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_usuario(id_usuario):
-   return Usuario.query.get(init(id_usuario))
+   return Usuario.query.get(int(id_usuario))
    
 class Usuario(database.Model, UserMixin):
    id = database.Column(database.Integer, primary_key=True)
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,7 +1,7 @@
 # criar as rotas do site
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from fakepinterest import app, database, bcrypt
-from flask_login import login_required
+from flask_login import login_required, login_user, logout_user
 from fakepinterest.forms import FormLogin, FormCriarConta
 from fakepinterest.models import Usuario, Foto
 
@@ -9,6 +9,11 @@ from fakepinterest.models import Usuario, Foto
 @app.route("/", methods=["GET", "POST"])
 def homepage():
    form_login = FormLogin()
+   if form_login.validate_on_submit():
+      usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+      if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+         login_user(usuario)
+         return redirect(url_for("perfil", usuario=usuario.username))   
    return render_template('index.html', form=form_login)
 
 
@@ -22,10 +27,17 @@ def criarconta():
                      email=form_criarconta.email.data)
       database.session.add(usuario)
       database.session.commit()
-     
+      login_user(usuario, remember=True)
+      return redirect(url_for("perfil", usuario=usuario.username))
    return render_template("criarconta.html", form=form_criarconta)
 
 @app.route("/perfil/<usuario>")
 @login_required
 def perfil(usuario):
    return render_template("perfil.html", usuario=usuario)
+
+@app.route("/logout")
+@login_required
+def logout():
+   logout_user()
+   return redirect(url_for("index.html"))
